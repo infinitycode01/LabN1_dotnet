d11be486562d818073650a30cafe06809edbf349
dmytro <infinity.code10@gmail.com>
2024-02-21 17:03:44 +0200

    .

--- obj/Debug/net8.0/LabN1_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/LabN1_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabN1_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f841b6b57f133f5f49c470f41536a7ca082566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ea056eb9e3732eaee8903a2c87befc46b82ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabN1_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabN1_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
